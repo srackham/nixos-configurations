0425629d7d3234ac35628e9080dfd729e359e7b9
Stuart Rackham <srackham@gmail.com>
2024-03-25 15:58:00 +1300

    gen=53   date=2024-03-25T02:56:56Z   nixos=24.05.20240314.d691274   kernel=6.6.21

--- configuration.nix
+++ configuration.nix
@@ -283,5 +283,4 @@
   # Refer to the following link for more details:
   # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
   nix.settings.auto-optimise-store = true;
-
 }

7417ced645457c93ca8c3825938d546aa793a393
Stuart Rackham <srackham@gmail.com>
2024-04-19 11:32:13 +1200

    add RP4 configuration.nix

---
+++ raspberry-pi-4/configuration.nix
@@ -0,0 +1,46 @@
+{ config, pkgs, lib, ... }:
+
+let
+  user = "guest";
+  password = "guest";
+  hostname = "rpi1";
+in {
+
+  boot = {
+    kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
+    initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
+    loader = {
+      grub.enable = false;
+      generic-extlinux-compatible.enable = true;
+    };
+  };
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
+  };
+
+  networking = {
+    networkmanager.enable = true;
+    hostName = hostname;
+  };
+
+  environment.systemPackages = with pkgs; [ vim ];
+
+  services.openssh.enable = true;
+
+  users = {
+    mutableUsers = false;
+    users."${user}" = {
+      isNormalUser = true;
+      password = password;
+      extraGroups = [ "wheel" ];
+    };
+  };
+
+  hardware.enableRedistributableFirmware = true;
+  system.stateVersion = "24.05";
+}

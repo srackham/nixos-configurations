82d5f3ed835958d09628b85bf7adec81f0a28c51
Stuart Rackham <srackham@gmail.com>
2024-04-20 13:27:20 +1200

    post install: only load GNOME bindings if GNOME is installed

--- extra/post-install-config.sh
+++ extra/post-install-config.sh
@@ -9,14 +9,16 @@ if [ ! -d "$HOME/.local/share/chezmoi" ]; then
     chezmoi init --apply https://github.com/srackham/dotfiles.git
 fi
 
-# Load GNOME desktop window manager custom key bindings. Created with:
-#
-#   dconf dump /org/gnome/desktop/wm/keybindings/ >keybindings.txt
-#
-dconf load /org/gnome/desktop/wm/keybindings/ <keybindings.txt
+if $(which dconf); then
+    # Load GNOME desktop window manager custom key bindings. Created with:
+    #
+    #   dconf dump /org/gnome/desktop/wm/keybindings/ >keybindings.txt
+    #
+    dconf load /org/gnome/desktop/wm/keybindings/ <keybindings.txt
 
-# Load GNOME desktop media custom key bindings. Created with:
-#
-#   dconf dump /org/gnome/settings-daemon/plugins/media-keys/ >media-keys.txt
-#
-dconf load /org/gnome/settings-daemon/plugins/media-keys/ <media-keys.txt
+    # Load GNOME desktop media custom key bindings. Created with:
+    #
+    #   dconf dump /org/gnome/settings-daemon/plugins/media-keys/ >media-keys.txt
+    #
+    dconf load /org/gnome/settings-daemon/plugins/media-keys/ <media-keys.txt
+fi

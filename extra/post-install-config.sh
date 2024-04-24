9e4fa19aa2f21d54562585db1d7b65dc6a193425
Stuart Rackham <srackham@gmail.com>
2024-04-25 08:33:56 +1200

    post-install.sh can be executed from anywhere

--- extra/post-install-config.sh
+++ extra/post-install-config.sh
@@ -4,6 +4,8 @@
 # for srackham that are not managed by configuration.nix
 #
 
+script_dir=$(dirname "${0}")
+
 # Create local chezmoi repo, pull from Github, then update dotfiles.
 if [ ! -d "$HOME/.local/share/chezmoi" ]; then
     echo installing dotfiles...
@@ -17,17 +19,17 @@ if command -v dconf >/dev/null 2>&1; then
     #
     #   dconf dump /org/gnome/desktop/wm/keybindings/ >keybindings.dconf
     #
-    dconf load /org/gnome/desktop/wm/keybindings/ < ./extra/keybindings.dconf
+    dconf load /org/gnome/desktop/wm/keybindings/ < $script_dir/keybindings.dconf
 
     # Load GNOME desktop media custom key bindings. Created with:
     #
     #   dconf dump /org/gnome/settings-daemon/plugins/media-keys/ >media-keys.dconf
     #
-    dconf load /org/gnome/settings-daemon/plugins/media-keys/ < ./extra/media-keys.dconf
+    dconf load /org/gnome/settings-daemon/plugins/media-keys/ < $script_dir/media-keys.dconf
 
     # Load GNOME Terminal profiles. Created with:
     #
     #   dconf dump /org/gnome/terminal/legacy/profiles:/ > gnome-terminal-profiles.dconf
     #
-    dconf load /org/gnome/terminal/legacy/profiles:/ < ./extra/gnome-terminal-profiles.dconf
+    dconf load /org/gnome/terminal/legacy/profiles:/ < $script_dir/gnome-terminal-profiles.dconf
 fi

24707a376b9f91b4a682c8fe9bdcc278b295a112
Stuart Rackham <srackham@gmail.com>
2024-04-27 16:03:29 +1200

    rpi1: cron job to copy local rsnapshot backups to removeable usb drive

--- rpi1-configuration.nix
+++ rpi1-configuration.nix
@@ -16,8 +16,11 @@
       # Email test.
       # "*/5 * * * *      srackham    echo Test email from cron"
 
+      # Copy local rsnapshot backups to removeable usb drive
+      "45 */8 * * * mount /dev/disk/by-label/BACKUPS /media/backups && rsync -aH --delete /files/backups /media/backups >/dev/null && umount /media/backups && logger -t data-backup -p user.info 'Backup rpi1:/backups to USB drive completed successfully' || logger -t data-backup -p user.err 'Backup rpi1:/backups to USB drive failed'"
+
       # Copy all data from nuc1:/files/ to rpi1:/files
-      "35 */4 * * *      root    rsync -avH --delete -e ssh --rsync-path='sudo rsync' --exclude '/aquota.*' nuc1:/files/ /files >/dev/null && logger -t data-backup -p user.info 'Backup nuc1:/files/ to rpi1 completed successfully' || logger -t data-backup -p user.err 'Backup nuc1:/files/ to rpi1 failed'"
+      "45 7,11,15,20 * * *      root    rsync -aH --delete -e ssh --rsync-path='sudo rsync' --exclude '/aquota.*' nuc1:/files/ /files >/dev/null && logger -t data-backup -p user.info 'Backup nuc1:/files/ to rpi1 completed successfully' || logger -t data-backup -p user.err 'Backup nuc1:/files/ to rpi1 failed'"
     ];
   };
 }

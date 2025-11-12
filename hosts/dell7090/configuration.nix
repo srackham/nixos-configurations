3ab699b41085d36800081ef53f96c0780afe1012
Stuart Rackham <srackham@gmail.com>
2025-11-13 06:46:27 +1300

    dell7090: ping Healthchecks.io at startup

--- hosts/dell7090/configuration.nix
+++ hosts/dell7090/configuration.nix
@@ -81,7 +81,8 @@
       # Commit today's notes
       "55 17 * * * srackham /home/srackham/bin/commit-notes"
 
-      # Ping Healthchecks.io
+      # Ping Healthchecks.io at startup and every 4 hours
+      "@reboot root sleep 30 && curl -fsS -m 10 --retry 5 -o /dev/null https://hc-ping.com/1f4df795-b3a7-4e69-9a02-dbf138bfcce9"
       "0 */4 * * * root curl -fsS -m 10 --retry 5 -o /dev/null https://hc-ping.com/1f4df795-b3a7-4e69-9a02-dbf138bfcce9"
     ];
   };

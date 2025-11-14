e60ac97adc1456667aae144cbbb6e46a380cf4b8
Stuart Rackham <srackham@gmail.com>
2025-11-14 17:54:42 +1300

    nixos: enable GNOME auto logon

--- modules/desktop-pc.nix
+++ modules/desktop-pc.nix
@@ -61,6 +61,8 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "srackham";
 
   # Configure keyboard (X11, Wayland and TTY compatible)
   services.keyd = {
